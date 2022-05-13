c8235b141c139f5b4ae95e04a866ed320703c234
Shishkin Evgeny <jovanny090691@gmail.com>
2022-05-13 10:42:02 +0300

    закомментил код и вышел, ахах))

--- js/main.js
+++ js/main.js
@@ -2,37 +2,37 @@
 
 //  РАНДОМНО ЗАПОЛНИТЬ 2 МАССИВА И ЗАПУЛИТЬ В 1 ПО ВОЗРАСТАНИЮ
 
-const   firstArr = [],
-        secondArr = [],
-        finalArr = [];
+// const   firstArr = [],
+//         secondArr = [],
+//         finalArr = [];
 
-let i = 0,
-    j = 0;
+// let i = 0,
+//     j = 0;
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min)) + min;
+// }
 
-for (let i=0;i<5;i++) {
-    firstArr.push(getRandomInt((i*10),(i*10+5))),
-    secondArr.push(getRandomInt((i*10),(i*10+5)));
-}
+// for (let i=0;i<5;i++) {
+//     firstArr.push(getRandomInt((i*10),(i*10+5))),
+//     secondArr.push(getRandomInt((i*10),(i*10+5)));
+// }
 
-while (i < firstArr.length || j < secondArr.length ) {
-    if (i>=firstArr.length) {
-    finalArr.push(secondArr[j++]);
-    } else if (j >= secondArr.length) {
-        finalArr.push(firstArr[i++])
-    } else if (secondArr[j] <= firstArr[i]) {
-        finalArr.push(secondArr[j++]);
-    } else {
-        finalArr.push(firstArr[i++]);
-    }
-}
+// while (i < firstArr.length || j < secondArr.length ) {
+//     if (i>=firstArr.length) {
+//     finalArr.push(secondArr[j++]);
+//     } else if (j >= secondArr.length) {
+//         finalArr.push(firstArr[i++])
+//     } else if (secondArr[j] <= firstArr[i]) {
+//         finalArr.push(secondArr[j++]);
+//     } else {
+//         finalArr.push(firstArr[i++]);
+//     }
+// }
 
-console.log(firstArr,secondArr,finalArr);
+// console.log(firstArr,secondArr,finalArr);
 
 // Cycle lesson
 
