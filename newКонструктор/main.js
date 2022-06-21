5790faa084bd1b4b8337a5359e3d9c9c430370a9
Shishkin Evgeny <jovanny090691@gmail.com>
2022-06-21 17:53:21 +0300

    отрисовка слайдера

--- newКонструктор/main.js
+++ newКонструктор/main.js
@@ -1,20 +1,57 @@
-function Picture (width,height,url) {
-   this.width = width;
-   this.height = height;
-   this.url = url;
+const Slider = {
+   images: ['https://www.itl.cat/pngfile/big/186-1861341_widescreen-road-scenery-hd.jpg',
+   'https://cdn.wallpapersafari.com/23/46/MctDLp.jpg',
+   'https://www.fonstola.ru/images/201112/fonstola.ru_64341.jpg'],
+   width: '500px',
+   height: '300px',
+   imgIndex: 0,
+   
 
-   this.createPicture = function () {
+   
+   createSliderWindow: function () {
       let div = document.createElement('div');
-      let img = document.createElement('img');
-      img.style.width = this.width;
-      img.style.height = this.height;
-      img.src = this.url;
+      div.style.width = this.width;
+      div.style.height = this.height;
+      div.className = 'container';
       document.body.append(div);
-      div.append(img);
-   }
+      div.append(this.createNavButton('left'));
+      div.append(this.addImgOnSlider());
+      div.append(this.createNavButton('right'));
+      document.body.append(this.createNavDots());
+
+      
+   },
+   createNavButton: function (side) {
+      let btn = document.createElement('div');
+      btn.className = 'btn';
+      btn.style.top = '120px';
+      btn.style[side] = '-70px';
+      btn.innerHTML = side;
+      
+      return btn
+   },
+   createNavDots: function () {
+      let dotList = document.createElement('ul');
+      dotList.className = 'dotList';
+      for (let i = 0; i < this.images.length;i++) {
+         let li = document.createElement('li');
+         li.className = 'dot'
+         dotList.append(li);
+
+      }
+      return dotList;
+   },
+   
+   addImgOnSlider: function () {
+      let img = document.createElement('img');
+      img.style.width = 'inherit';
+      img.style.height = 'inherit';
+      img.src = this.images[this.imgIndex];
+
+      return img;
+   },
+   
+      
 }
 
-let catPicture = new Picture ('300px','200px','http://mobimg.b-cdn.net/v3/fetch/2c/2c38ec7c72e3d0094f591d6f735a3b8e.jpeg');
-catBlock.createPicture();
-let dogPicture = new Picture ('300px','200px','https://otkritkis.com/wp-content/uploads/2022/01/17_977773.jpg');
-dogBlock.createPicture();
+Slider.createSliderWindow();
