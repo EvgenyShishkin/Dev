af6c293f7f2de7899cc99ce90d2f676515dc642b
Shishkin Evgeny <jovanny090691@gmail.com>
2022-06-23 16:08:59 +0300

    slider &todoList

--- newКонструктор/main.js
+++ newКонструктор/main.js
@@ -1,32 +1,31 @@
-function Slider (w,h,u) {
-   this.images = u;
-   this.width = w+'px';
-   this.height = h+'px';
+function Slider (width,height,url,) {
+   this.images = url;
+   this.width = width+'px';
+   this.height = height+'px';
    this.imgIndex = 0;
+   
    
 
    
-   this.createSliderWindow = function (pos) {
+   this.createSliderWindow = function (pos,) {
       let div = document.createElement('div');
       div.style.width = this.width;
       div.style.height = this.height;
       div.className = 'container';
       document.body.append(div);
-      div.append(this.createNavButton('left',h));
+      div.append(this.createNavButton('left',height));
       div.append(this.addImgBlock());
-      this.setImage(this.imgIndex);
-      div.append(this.createNavButton('right',h));
+      div.append(this.createNavButton('right',height));
       document.body.append(this.createNavDots());
+      this.setImageAndSetActiveDot(this.imgIndex);
       
-      
-
       
    };
-   this.createNavButton = function (side,h) {
+   this.createNavButton = function (side,height) {
       let btn = document.createElement('div');
       btn.className = 'btn';
       btn.dataset.btn = side;
-      btn.style.top = h/2-30+"px";
+      btn.style.top = height/2-30+"px";
       btn.style[side] = '-70px';
       btn.innerHTML = side;
       btn.addEventListener('click', () => {
@@ -40,53 +39,57 @@ function Slider (w,h,u) {
       return btn
    };
    this.createNavDots = function () {
-      let dotList = document.createElement('ul');
-      dotList.className = 'dotList';
+      this.dotList = document.createElement('ul');
+      this.dotList.className = 'dotList';
       for (let i = 0; i < this.images.length;i++) {
          let li = document.createElement('li');
          li.className = 'dot'
-         dotList.append(li);
+         this.dotList.append(li);
 
       }
-      return dotList;
+      return this.dotList;
    };
    
    this.addImgBlock = function () {
-      let img = document.createElement('img');
-      img.style.width = 'inherit';
-      img.style.height = 'inherit';
-      img.className = 'sliderImage';
+      this.img = document.createElement('img');
+      this.img.style.width = 'inherit';
+      this.img.style.height = 'inherit';
+      this.img.className = 'sliderImage';
       
-      return img;
+      return this.img;
    };
-   this.setImage = function (pos) {
-      let img = document.querySelector('.sliderImage');
-      img.src = this.images[pos];
-      // document.body.style.backgroundImage = `url(${this.images[pos]})`
+   this.setActiveDot = function () {
+      for(let i = 0;i < this.images.length;i++) {
+         if (i === this.imgIndex) {
+            this.dotList.children[i].className = "dot--active";
+         } else {
+            this.dotList.children[i].className = "dot";          
+         }
+      }
+   };
+   this.setImageAndSetActiveDot = function (pos) {
+      this.img.src = this.images[pos];
+      this.setActiveDot();
    };
    this.nextBtn = function () {
       if(this.imgIndex < this.images.length-1) {
-         this.setImage(++this.imgIndex)
-         
+         this.setImageAndSetActiveDot(++this.imgIndex)
    };
    this.prevBtn = function () {
       if(this.imgIndex > 0) {
-         this.setImage(--this.imgIndex);
-         
+         this.setImageAndSetActiveDot(--this.imgIndex);
       }
    };
-   this.setActiveDot = function () {
-      
-   } 
+   
   
 };
 }
 
-// let slider1 = new Slider (500,300,['https://happyn.ru/uploads/posts/2019-11/thumbs/1573568668_75.jpg',
-// 'https://live.staticflickr.com/2948/15465273181_b9907aed39.jpg',
-// 'https://lasix.ru/nw/images/500x300/31108.jpg',
-// 'https://www.astromeridian.ru/assets/images/sonnik/podsolnyxi.jpg']);
-// slider1.createSliderWindow(1);
+let slider1 = new Slider (500,300,['https://happyn.ru/uploads/posts/2019-11/thumbs/1573568668_75.jpg',
+'https://live.staticflickr.com/2948/15465273181_b9907aed39.jpg',
+'https://lasix.ru/nw/images/500x300/31108.jpg',
+'https://www.astromeridian.ru/assets/images/sonnik/podsolnyxi.jpg']);
+slider1.createSliderWindow(1);
 
 let slider2 = new Slider (720,480,['https://gidfon.com/prev/21460.jpg',
 'https://storage.theoryandpractice.ru/tnp/uploads/course_image_unit/000/000/494/image/base_fe777f7405.jpg',
